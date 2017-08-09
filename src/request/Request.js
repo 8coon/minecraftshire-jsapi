f46f2d33a9e7bd55421698096b217aab1a863c09
Kaibito <gpuzzletime@icloud.com>
2017-08-09 23:41:07 +0300

    Added events to Request

--- src/request/Request.js
+++ src/request/Request.js
@@ -1,14 +1,22 @@
 
-export const RequestParams = {
+export var RequestParams = {
     backendUrl: 'https://minecraftshire.ru/api/',
     authToken: '',
     auth: true,
 };
 
 
+export var RequestEvent = {
+    SEND: 'send',
+    ERROR: 'error',
+    SUCCESS: 'success',
+};
+
+
 export default {
 
     defaultParams: RequestParams,
+    listeners: {},
 
     /**
      * Make request to backend
@@ -27,12 +35,15 @@ export default {
             payload.authToken = params.authToken;
         }
 
+        var _this = this;
         return new Promise(function(resolve, reject) {
-            const xhr = new XMLHttpRequest();
+            var xhr = new XMLHttpRequest();
 
             xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.withCredentials = true;
             xhr.send(JSON.stringify(payload));
+            _this.emit(RequestEvent.SEND, {xhr: xhr, request: _this, payload: payload});
 
             xhr.onreadystatechange = function() {
                 if (xhr.readyState !== 4) {
@@ -40,13 +51,81 @@ export default {
                 }
 
                 if (xhr.status < 300) {
+                    _this.emit(RequestEvent.SUCCESS, {xhr: xhr, request: _this, payload: payload});
                     resolve(xhr);
                     return;
                 }
 
+                _this.emit(RequestEvent.ERROR, {xhr: xhr, request: _this, payload: payload});
                 reject(xhr);
             }
         });
     },
+
+
+    /**
+     * Subscribe listener to certain Request event.
+     * @param {string} type
+     * @param {function|object} listener
+     * @param {string|null} handlerName = 'handleEvent'
+     * @param {string|null} key listener identifier.
+     */
+    on(type, listener, handlerName, key) {
+        this.listeners[type] = this.listeners[type] || [];
+        this.listeners[type].push({
+            key: key || listener,
+            handlerName: handlerName,
+            listener: listener,
+        });
+    },
+
+
+    /**
+     * Unsubscribe provided listener
+     * @param {string} type
+     * @param {function|object|string} listener
+     */
+    off(type, listener) {
+        var listeners = this.listeners[type];
+        if (!listeners) return;
+
+        for (var i = 0; i < listeners.length; i++) {
+            if (listener === listeners[i]) {
+                delete listeners[i];
+                break;
+            }
+        }
+
+        if (listeners.length === 0) delete this.listeners[type];
+    },
+
+
+    /**
+     * Emit event
+     * @param {string} type
+     * @param {object} details
+     */
+    emit(type, details) {
+        var listeners = this.listeners[type];
+        if (!listeners) return;
+
+        var event = {
+            type: type,
+            details: details,
+        };
+
+        for (var i = 0; i < listeners.length; i++) {
+            var listenerObj = listeners[i],
+                listener = listenerObj.listener,
+                handler = listenerObj.handlerName,
+                callback = typeof listener === 'function' ? listener : listener[handler || 'handleEvent'];
+
+            if (!callback || typeof callback !== 'function') {
+                continue;
+            }
+
+            callback(event);
+        }
+    }
 
 }
